54008298b567b95de7fd835a3ecfae740fc226c5
subhmaharana <subhransumaharana@gmail.com>
2019-01-29 21:02:02 +0530

    Angular Websocket client for Spring boot - Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StompService } from '@stomp/ng2-stompjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ang-websocket-client-app';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'Websocket demo';
+  private subs: Subscription;
+
+  constructor(private stompService: StompService) { }
+
+  ngOnInit() {
+    this.subs = this.stompService.subscribe('/chat').subscribe((data) => {
+      console.log('subscribed to chat..', data);
+    });
+  }
+
+  sendMessage() {
+    const outGoingMessage = JSON.stringify({ message: 'Hello from the shubh client' });
+    this.stompService.publish('/app/send/message', outGoingMessage);
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StompService, StompConfig } from '@stomp/ng2-stompjs';
+
+const stompConfig: StompConfig = {
+  // added '/websocket' for spring boot SockJS
+  url: 'ws://127.0.0.1:8080/shubh-chat-app/websocket',
+  headers: {
+    login: 'guest',
+    passcode: 'guest'
+  },
+  heartbeat_in: 0,
+  heartbeat_out: 20000, // 20000 - every 20 seconds
+  reconnect_delay: 5000,
+  debug: true
+};
+
+
+
 
 @NgModule({
   declarations: [
@@ -12,7 +29,13 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    StompService,
+    {
+      provide: StompConfig,
+      useValue: stompConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
